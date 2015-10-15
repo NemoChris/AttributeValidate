725b1997f68b4b3ad38ea5fa0cc13ecb4a85d113
hpy <765123735@qq.com>
2015-10-15 17:35:01 +0800

    代码实现 v0.1

---
+++ AttributeValidate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AttributeValidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 模拟的数据
+            string userName = string.Empty;
+
+            // 对数据进行验证
+            var user = new User()
+                           {
+                               UserName = userName
+                           };
+            var res= user.Validate();
+            
+        }
+    }
+}
---
+++ AttributeValidate/RequiredAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AttributeValidate
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class RequiredAttribute : Attribute
+    {
+        /// <summary>
+        /// 验证失败返回的错误信息
+        /// </summary>
+        public string ErrorMessage
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 输入值
+        /// </summary>
+        public object InputValue
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 验证规则
+        /// </summary>
+        /// <param name="errorMessage">验证失败返回的错误信息</param>
+        public RequiredAttribute(string errorMessage)
+        {
+            this.ErrorMessage = errorMessage;
+        }
+
+        /// <summary>
+        /// 接口参数为空或空字符串都返回false
+        /// </summary>
+        /// <returns></returns>
+        public  bool Validate()
+        {
+            if (InputValue == null || string.IsNullOrWhiteSpace(InputValue.ToString()))
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
