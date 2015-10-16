7eae7c98cf64d9f400629b5e552d18640e1adb2d
hpy <765123735@qq.com>
2015-10-16 16:51:30 +0800

    验证结果类

---
+++ AttributeValidate/ValidateResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AttributeValidate
+{
+    public class ValidateResult
+    {
+        public bool IsSuc { get; set; }
+        public string ResultMessage { get; set; }
+        public ValidateCode Code { get; set; }
+    }
+
+    public  enum  ValidateCode
+    {
+        /// <summary>
+        /// Require特性验证失败
+        /// </summary>
+        RequireFailed=101,
+        /// <summary>
+        /// Regex特性验证失败
+        /// </summary>
+        RegexFailed=102,
+        /// <summary>
+        /// 验证成功
+        /// </summary>
+        OK=200
+    }
+}
