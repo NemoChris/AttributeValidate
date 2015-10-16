ce24dd606dde177281d42c2f40fdd7a5f43c4307
hpy <765123735@qq.com>
2015-10-16 14:35:36 +0800

    model抽象父类BaseModel

---
+++ AttributeValidate/BaseModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AttributeValidate
+{
+    public abstract class BaseModel
+    {
+        public virtual string Validate()
+        {
+            // 反射取当前对象的所有属性
+            System.Reflection.PropertyInfo[] properties = this.GetType().GetProperties();
+            foreach (var pInfo in properties)
+            {
+                // 取属性中的特性
+                if (pInfo.IsDefined(typeof(RequiredAttribute), false))
+                {
+                    var customAttributes =
+                        pInfo.GetCustomAttributes(typeof(RequiredAttribute), false) as RequiredAttribute[];
+                    // 根据特性进行参数验证
+                    foreach (var attribute in customAttributes)
+                    {
+                        // 初始化特性，并进行验证。
+                        var validateVal = pInfo.GetValue(this, null);
+                        attribute.InputValue = validateVal;
+                        if (!attribute.Validate())
+                        {
+                            return "验证失败！消息：" + attribute.ErrorMessage;
+                        }
+                    }
+                }
+            }
+            // 验证成功
+            return "验证成功!";
+        }
+    }
+}
