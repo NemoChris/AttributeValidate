2675df3dfca8becc733a97c997c814f91b8f0923
Nemo <765123735@qq.com>
2015-10-17 11:34:38 +0800

    添加bll方法。

---
+++ AttributeValidate/BLL/UserBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AttributeValidate.BLL
+{
+    public class UserBll
+    {
+        public static bool IsExixtUser(string userName)
+        {
+            // 模拟数据库数据
+            var userNameList=new List<string>(){"admin","hpy"};
+            return userNameList.Exists(u => u == userName);     
+        }
+    }
+}
