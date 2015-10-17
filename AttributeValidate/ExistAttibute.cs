34c68833f3a13c2d0348779b8e72c312a02fbffc
Nemo <765123735@qq.com>
2015-10-17 11:28:10 +0800

    添加Exist特性类。

---
+++ AttributeValidate/ExistAttibute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AttributeValidate
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExistAttibute:ValidateAttibute
+    {
+        public Func<bool> IsExistDataCallBack { get; set; }
+        public ExistAttibute(Func<bool> callBack,string errorMessage) : base(errorMessage)
+        {
+            this.IsExistDataCallBack = callBack;
+            this.ErrorType=ValidateCode.ExistFailed;
+        }
+
+        public override bool Validate()
+        {
+            if (this.IsExistDataCallBack==null)
+            {
+                throw new NullReferenceException("验证方法不能为空。");
+            }
+            return this.IsExistDataCallBack();
+        }
+    }
+}
