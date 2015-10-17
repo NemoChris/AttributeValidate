7f5107b6665ed42dd6c789666046364c369a54d2
Nemo <765123735@qq.com>
2015-10-17 10:46:11 +0800

    添加正则验证类

---
+++ AttributeValidate/RegexAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AttributeValidate
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RegexAttribute:ValidateAttibute
+    {
+        public string RegexStr { get; set; }
+        public RegexAttribute(string errorMessage) : base(errorMessage)
+        {
+this.ErrorType=ValidateCode.RegexFailed;
+        }
+
+        public override bool Validate()
+        {
+            return Regex.IsMatch(this.InputValue.ToString(), this.RegexStr);
+        }
+    }
+}
