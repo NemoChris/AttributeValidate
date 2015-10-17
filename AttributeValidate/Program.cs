3310182071484be3a737b9fb7c2ba6a27dae726a
Nemo <765123735@qq.com>
2015-10-17 10:55:02 +0800

    增加测试代码

--- AttributeValidate/Program.cs
+++ AttributeValidate/Program.cs
@@ -9,19 +9,15 @@ namespace AttributeValidate
     {
         static void Main(string[] args)
         {
-            // 模拟的数据
-            string userName = string.Empty;
 
             // 对数据进行验证
-            var user = new User()
-                           {
-                               UserName = userName
-                           };
+            var user = new User();
+            user.UserName = "hpy";
+            user.QQ = "01234";
             var res= user.Validate();
             // 正常情况
             user.UserName = "admin";
             var res2 = user.Validate();
-
         }
     }
 }
